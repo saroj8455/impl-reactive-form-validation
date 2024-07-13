312b604bdde38d69a227c80747ad19046bcad380
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-07-13 15:06:48 +0530

    implement trim custom validator for start and end with space

--- src/app/components/user-info/user-info.component.ts
+++ src/app/components/user-info/user-info.component.ts
@@ -1,9 +1,11 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import {
+  AbstractControl,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
+  ValidationErrors,
   Validators,
 } from '@angular/forms';
 
@@ -21,16 +23,34 @@ export class UserInfoComponent {
     this.userForm = new FormGroup({
       firstname: new FormControl('', [
         Validators.required,
+        trimValidator,
         Validators.minLength(5),
       ]),
       lastname: new FormControl('', [
         Validators.required,
+        trimValidator,
         Validators.minLength(5),
       ]),
     });
   }
 
   validateUserInput() {
-    console.warn(this.userForm.value);
+    const userInfo = this.userForm.value
+    console.warn(userInfo);
+    alert(`Hi! ${userInfo.firstname} ${userInfo.lastname}`);
   }
 }
+
+
+
+export const trimValidator = (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value;
+
+    if (!value || value === '') return null;
+
+    if (value.startsWith(' ') || value.endsWith(' ')) {
+        control.setValue(control.value?.trim(), { emitEvent: false, onlySelf: true });
+    }
+
+    return null;
+};
